4238a0722d823f49774ffb9fb537032415bd76a6
Joshua Ackley <jackley798@gmail.com>
2017-10-21 22:05:05 -0400

    Everything but GUI

--- Movie.py
+++ Movie.py
@@ -1,15 +1,15 @@
 class Movie:
-    def __init__(self, title, movie_genre_ids, favorite_genres):
-        self.title = title
+    def __init__(self, movie_title, movie_genre_ids, favorite_genres):
+        self.title = movie_title
         self.genre_ids = movie_genre_ids
-        self.favorite_genres = favorite_genres
+        self.user_genres = favorite_genres
         self.score = self.compute_score
 
     def compute_score(self):
         score = 0
         number_of_genres = len(self.genre_ids)
         for i in range(0,16):
-            if self.favorite_genres[i][2] in self.genre_ids:
-                score += self.favorite_genres[i][1]
+            if self.user_genres[i][2] in self.genre_ids:
+                score += self.user_genres[i][1]
         score /= number_of_genres
         return score
--- hackathon.py
+++ hackathon.py
@@ -4,44 +4,42 @@ import configparser
 import praw
 import re
 import twitter
+import watson_developer_cloud
 from watson_developer_cloud import PersonalityInsightsV3
 import requests
 import urllib
-import Movie
 
 config = configparser.ConfigParser()
 config.read('config.cfg')
 
-
 # Takes configparser as argument and connects Twitter API to account
 
-def authenticate_twitter(config_file):
-    consumer_key = str(config_file['Twitter']['consumer key'])
-    consumer_secret = str(config_file['Twitter']['consumer secret'])
-    access_token = str(config_file['Twitter']['access token'])
-    access_token_secret_user = str(config_file['Twitter']['access token secret'])
+def authenticateTwitter(config):
+    consumerKey = str(config['Twitter']['consumer key'])
+    consumerSecret = str(config['Twitter']['consumer secret'])
+    accessToken = str(config['Twitter']['access token'])
+    accessTokenSecret = str(config['Twitter']['access token secret'])
 
-    twitter_api = twitter.Api(consumer_key=consumer_key,
-                              consumer_secret=consumer_secret,
-                              access_token_key=access_token,
-                              access_token_secret=access_token_secret_user)
-    return twitter_api
-
+    twitterApi = twitter.Api(consumer_key=consumerKey,
+                             consumer_secret=consumerSecret,
+                             access_token_key=accessToken,
+                             access_token_secret=accessTokenSecret)
+    return twitterApi
 
 # Takes Twitter API as argument and fetches all tweets by user.
 
-def fetch_tweets(api):
-    status_text = ""
-    status_set = api.GetUserTimeline(exclude_replies=False, include_rts=False)
-    while len(status_set) > 0:
-        last_status_id = status_set[len(status_set) - 1].id
-        for status in status_set:
-            status_text += " " + status.text
-        status_set = api.GetUserTimeline(exclude_replies=False,
-                                         include_rts=False,
-                                         max_id=last_status_id - 1)
+def fetchTweets(api):
+    statusText = ""
+    statusSet = api.GetUserTimeline(exclude_replies=False, include_rts=False)
+    while len(statusSet) > 0:
+        lastStatusID = statusSet[len(statusSet) - 1].id
+        for status in statusSet:
+            statusText += " " + status.text
+        statusSet = api.GetUserTimeline(exclude_replies=False,
+                                        include_rts=False,
+                                        max_id=lastStatusID - 1)
 
-    return status_text
+    return statusText
 
 
 def get_traits_dic(prof):
@@ -64,72 +62,65 @@ Takes Reddit API and configparser as arguments and fetches all reddit comments
 made by the user.
 '''
 
-
-def fetch_reddit_comments(api, user_config):
-    reddit_comment_text = ""
-    redditor = api.redditor(str(user_config['Reddit']['username']))
+def fetchRedditComments(api, config):
+    redditCommentText = ""
+    redditor = api.redditor(str(config['Reddit']['username']))
     for comment in redditor.comments.new(limit=None):
-        reddit_comment_text += " " + comment.body
+        redditCommentText += " " + comment.body
 
-    return reddit_comment_text
-
+    return redditCommentText
 
 # Takes configparser as argument and connects Reddit API to account.
 
-def authenticate_reddit(user_config):
-    client_id = str(user_config['Reddit']['client id'])
-    client_secret = str(user_config['Reddit']['client secret'])
-    password = str(user_config['Reddit']['password'])
-    username = str(user_config['Reddit']['username'])
-    reddit_api = praw.Reddit(client_id=client_id,
-                             client_secret=client_secret,
-                             password=password,
-                             user_agent='script by /u/alexwaibel',
-                             username=username)
-    return reddit_api
-
+def authenticateReddit(config):
+    clientId = str(config['Reddit']['client id'])
+    clientSecret = str(config['Reddit']['client secret'])
+    password = str(config['Reddit']['password'])
+    username = str(config['Reddit']['username'])
+    redditApi = praw.Reddit(client_id=clientId,
+                            client_secret=clientSecret,
+                            password=password,
+                            user_agent='script by /u/alexwaibel',
+                            username=username)
+    return redditApi
 
 def main():
+    twitterEnabled = config['Twitter'].getboolean('enabled')
+    redditEnabled = config['Reddit'].getboolean('enabled')
 
-    combined_text = ''
-    twitter_enabled = config['Twitter'].getboolean('enabled')
-    reddit_enabled = config['Reddit'].getboolean('enabled')
-
-    if twitter_enabled:
-        twitter_api = authenticate_twitter(config)
-        tweet_text = fetch_tweets(twitter_api)
+    if twitterEnabled:
+        twitterApi = authenticateTwitter(config)
+        tweetText = fetchTweets(twitterApi)
         # Remove all mentions from tweets.
-        tweet_text = re.sub('(@[A-Za-z0-9]+)',
-                            '', tweet_text, flags=re.MULTILINE)
+        tweetText = re.sub('(@[A-Za-z0-9]+)',
+                           '', tweetText, flags=re.MULTILINE)
         # Remove all URLs from tweets.
-        tweet_text = re.sub(r'http\S+', '', tweet_text, flags=re.MULTILINE)
-        combined_text += tweet_text
+        tweetText = re.sub(r'http\S+', '', tweetText, flags=re.MULTILINE)
 
-    if reddit_enabled:
-        reddit_api = authenticate_reddit(config)
-        reddit_comment_text = fetch_reddit_comments(reddit_api, config)
+    if redditEnabled:
+        redditApi = authenticateReddit(config)
+        redditCommentText = fetchRedditComments(redditApi, config)
 
         # Remove URLs from reddit comments.
-        reddit_comment_text = re.sub(r'http\S+', '', reddit_comment_text,
-                                     flags=re.MULTILINE)
-        combined_text += reddit_comment_text
+        redditCommentText = re.sub(r'http\S+', '', tweetText,
+                                   flags=re.MULTILINE)
 
     personality_insights = PersonalityInsightsV3(
         version='2017-10-13',
         username='136e3f1c-5154-41bb-8a3a-dda7edb1e118',
         password='TZlDSGcxPVVf')
 
-    # Gets personality breakdown from watson as a JSON(?)
-    user_prof = personality_insights.profile(combined_text,
-                                             content_type='text/plain',
-                                             content_language=None,
-                                             accept='application/json',
-                                             accept_language=None,
-                                             raw_scores=True,
-                                             consumption_preferences=True,
-                                             csv_headers=False)
-    trait_dictionary = get_traits_dic(user_prof)
-    consum_dictionary = get_consumption_traits(user_prof)
+    #Gets personality breakdown from watson as a JSON(?)
+    userProf = personality_insights.profile(tweetText,
+                                            content_type='text/plain',
+                                            content_language=None,
+                                            accept='application/json',
+                                            accept_language=None,
+                                            raw_scores=True,
+                                            consumption_preferences=True,
+                                            csv_headers=False)
+    trait_dictionary = get_traits_dic(userProf)
+    consum_dictionary = get_consumption_traits(userProf)
 
     print(trait_dictionary)
     print(consum_dictionary)
@@ -145,7 +136,6 @@ def main():
     print(ext)
     print(agr)
     print(neu)
-
 
     action_score = ((1-ope)+ext+consum_dictionary["action"]) / 3
     adventure_score = (con+agr+consum_dictionary["adventure"]) / 3
@@ -176,14 +166,11 @@ def main():
     movie_db_api = "https://api.themoviedb.org"
     movies = list()
 
-
     for i in range(1,11):
         url = movie_db_api + "/3/discover/movie?api_key="+movie_db_apikey+"&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=true&page="+str(i)+"&with_genres="+str(scores[0][2])+"|"+str(scores[1][2])+"|"+str(scores[2][2])+"|"+str(scores[3][2])+"&with_original_language=en"
         response = requests.get(url)
         json_data = response.json()
-        for i in range(0, 20):
-            mymovie = Movie(json_data['results']['title'], json_data['results']['genre_ids'], scores)
-            movies.append([("title",json_data['results']['title']), ('genre_ids', json_data['results']['genre_ids'])])
+        movies.append(json_data['results'])
 
     print(movies)
 
